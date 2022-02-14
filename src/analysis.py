e35d84ce772d10b113b632a702fffc570a22dc27
Michael Christian <chrispit@ou.edu>
2022-02-14 15:49:41 -0500

    now reads multiple files from command line graphs are now titled with name of data file

--- src/analysis.py
+++ src/analysis.py
@@ -1,5 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+
+# time axis is nano seconds
+
+time_out = 40000 # any time after this isn't recorded, they should be ignored
 
 ## Reads a data file and returns a sorted array of valid times
 def parseFile(file):
@@ -11,16 +16,20 @@ def parseFile(file):
     ## read line by line and grab any data less than 4000
     for line in lines:
         numbers = line.split()
-        if int(numbers[0]) < 40000: #TODO move out magic number
+        if int(numbers[0]) < time_out: #TODO move out magic number(done?)
             valid_times.append(int(numbers[0]))
 
     valid_times.sort()
+
+## DEBUG
+    ##print(valid_times)
+##
     return valid_times
 
 ## Put our data in to bins of a given size
 def binData(dataSet, binsize):
     ## 4000 is a max, so we'll break up the data set into chunks based on binsize
-    bincount = int(40000 / binsize)
+    bincount = int(time_out / binsize)
     counter = {}
 
     ## So counter will be a dict counting how man data points were in each bin
@@ -45,22 +54,29 @@ def binData(dataSet, binsize):
     return counter
 
 ## Data is expected to be a dict of type int, int
-def plotData(data):
+def plotData(data, file_name):
     ## matplotlib boilerplate
     xdata, ydata = zip(*data.items())
 
     x = np.array(xdata)
     y = np.array(ydata)
 
-    plt.plot(x, y, linestyle='--')
+    plt.plot(x, y)
+
+    plt.title(file_name)
 
     ## an expontial fucntion to check our data
 
     plt.show()
 
-def main(file, binsize):
-    times = parseFile(file)
-    binnedData = binData(times, binsize)
-    plotData(binnedData)
+def main(files, binsize):
+    for file in files :
+        times = parseFile(str(file))
+        binnedData = binData(times, binsize)
+        plotData(binnedData, str(file))
 
-main('../data/muon 1-31-21.data', 50)
+# the device measures in increments of 20, so if the bin size is not a multiple of 20
+# the bins get skewed
+# we can change the bin number in the measuring program
+
+main(sys.argv[1:], 20)
